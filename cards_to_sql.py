484f3855344dc6276be89fd66826bdbce6890f90
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2020-08-05 16:46:53 -0500

    Added a deck
    Cards Against Security

--- cards_to_sql.py
+++ cards_to_sql.py
@@ -4,6 +4,8 @@ hah_black_cards_file = open("hah_black.txt")
 hah_white_cards_file = open("hah_white.txt")
 cai_black_cards_file = open("cai_black.txt")
 cai_white_cards_file = open("cai_white.txt")
+cas_black_cards_file = open("cas_black.txt")
+cas_white_cards_file = open("cas_white.txt")
 sql_file = open("pyx.sql", "w")
 # (pack_id, draw, pick, text, watermark)
 black_cards = []
@@ -11,19 +13,24 @@ black_cards = []
 white_cards = []
 # (id, active, base_deck, description, name, wight)
 hah_deck = (1, 1, 0, "Hackers Against Humanity deck by Vegas 2.0", "Hackers Against Humanity", 1)
-cai_deck = (2, 1, 0, "Cards Against Infosec expansion for Cards Against IT", "Cards Against Infosec", 1)
+cai_deck = (2, 1, 0, "Cards Against Infosec expansion for Cards Against IT by Varonis", "Cards Against Infosec", 1)
+cas_deck = (3, 1, 0, "Cards Against Security", "Cards Against Security", 1)
 
 # Read in the black cards
 for draw,pick,text in [line.strip().split("~") for line in hah_black_cards_file.readlines()]:
     black_cards.append((1, int(draw), int(pick), text, "HAH"))
 for draw,pick,text in [line.strip().split("~") for line in cai_black_cards_file.readlines()]:
     black_cards.append((2, int(draw), int(pick), text, "CAI"))
+for draw,pick,text in [line.strip().split("~") for line in cas_black_cards_file.readlines()]:
+    black_cards.append((3, int(draw), int(pick), text, "CAS"))
 
 # Read in the white cards
 for line in hah_white_cards_file.readlines():
     white_cards.append((1, line.strip(), "HAH"))
 for line in cai_white_cards_file.readlines():
     white_cards.append((2, line.strip(), "CAI"))
+for line in cas_white_cards_file.readlines():
+    white_cards.append((3, line.strip(), "CAS"))
 
 # Begin the splqite transaction
 sql_file.write("PRAGMA foreign_keys=OFF;\n")
@@ -48,6 +55,7 @@ for i in range(len(white_cards)):
 sql_file.write("CREATE TABLE card_set (id integer, active integer not null, base_deck integer not null, description varchar, name varchar, weight integer not null, primary key (id));\n")
 sql_file.write(f"INSERT INTO card_set VALUES({hah_deck[0]},{hah_deck[1]},{hah_deck[2]},'{hah_deck[3]}','{hah_deck[4]}',{hah_deck[5]});\n")
 sql_file.write(f"INSERT INTO card_set VALUES({cai_deck[0]},{cai_deck[1]},{cai_deck[2]},'{cai_deck[3]}','{cai_deck[4]}',{cai_deck[5]});\n")
+sql_file.write(f"INSERT INTO card_set VALUES({cas_deck[0]},{cas_deck[1]},{cas_deck[2]},'{cas_deck[3]}','{cas_deck[4]}',{cas_deck[5]});\n")
 
 # Create card_set_black_card table
 sql_file.write("CREATE TABLE card_set_black_card (card_set_id integer not null, black_card_id integer not null, primary key(card_set_id, black_card_id));\n")
@@ -60,6 +68,8 @@ for i in range(len(white_cards)):
     sql_file.write(f"INSERT INTO card_set_white_card VALUES({white_cards[i][0]},{i+1});\n")
 
 sql_file.close()
+cas_white_cards_file.close()
+cas_black_cards_file.close()
 cai_white_cards_file.close()
 cai_black_cards_file.close()
 hah_white_cards_file.close()
